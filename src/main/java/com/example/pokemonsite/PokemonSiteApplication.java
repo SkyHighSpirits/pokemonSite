97f86ff5c2ef176411d6dd63e4a42376ddb70470
Nicolai Ene <35382666+SkyHighSpirits@users.noreply.github.com>
2023-03-17 14:03:15 +0100

    Initial commit

---
+++ src/main/java/com/example/pokemonsite/PokemonSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokemonsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pokemonsite/PokemonSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemonsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
